9ddfb5efa2b16735040b006323c25612b8170e72
tomasvikner <tomas.vikner@gmail.com>
2025-11-20 04:57:11 -0600

    Cleanup

--- prepFolders.m
+++ prepFolders.m
@@ -1,23 +1,16 @@
-vold = '/Volumes/radiology';
-groups = fullfile(vold, 'Groups');
-group = fullfile(groups, 'CVMRIGroup');
-users = fullfile(group, 'Users');
-user = fullfile(users, 'txv016');
-wrap2 = fullfile(user, 'WRAP2');
-niid = fullfile(wrap2, 'niis', 'niis');
-base_dir = [];
-base_dir = niid;
-dcp = dir(base_dir);
-dcp = dcp(~ismember({dcp.name}, {'.','..'}));
 
-outdir = '/Users/TXV016/SaveToRad/WRAP2PROC25/';
-if ~exist(outdir, 'dir')
-    mkdir(outdir)
-end
-for i = 1:numel(dcp)
-    dn = dcp(i).name;
-    outfolder = fullfile(outdir, dn);
-    if ~exist(outfolder, 'dir')
-        mkdir(outfolder)
-    end
-end
+% dcp = dir(base_dir);
+% dcp = dcp(~ismember({dcp.name}, {'.','..'}));
+% 
+% outdir = 'TEMP';
+% if ~exist(outdir, 'dir')
+%     mkdir(outdir)
+% end
+
+% for i = 1:numel(dcp)
+%     dn = dcp(i).name;
+%     outfolder = fullfile(outdir, dn);
+%     if ~exist(outfolder, 'dir')
+%         mkdir(outfolder)
+%     end
+% end
